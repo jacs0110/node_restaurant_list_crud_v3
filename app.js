5d17f89e337df18f2882599658ea396b785fb9cc
jacs <wbruce0110@gmail.com>
2019-06-22 11:10:13 +0800

    create edit page and function

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ const restaurantList = require('./models/restaurantList.js')
 app.use(bodyPaser.urlencoded({ extended: true }))
 
 // routes
+// Go to index page
 app.get('/', (req, res) => {
   restaurantList.find((err, restaurants) => {
     if (err) return console.error(err)
@@ -39,6 +40,7 @@ app.get('/', (req, res) => {
   })
 })
 
+// Go to show page
 app.get('/restaurant/:id', (req, res) => {
   restaurantList.findById(req.params.id, (err, restaurant) => {
     if (err) console.error(err)
@@ -46,12 +48,13 @@ app.get('/restaurant/:id', (req, res) => {
   })
 })
 
+// Go to creating a new restaurant page
 app.get('/restaurants/new', (req, res) => {
   console.log('create a new item')
   res.render('new')
 })
 
-// create a new todo
+// create a new restaurant
 app.post('/restaurants', (req, res) => {
   console.log(req.body)
   const newRestaurant = restaurantList({
@@ -70,7 +73,35 @@ app.post('/restaurants', (req, res) => {
     if (err) console.error(err)
     res.redirect('/')
   })
+})
 
+// Go to edit page
+app.get('/restaurant/:id/edit', (req, res) => {
+  restaurantList.findById(req.params.id, (err, restaurant) => {
+    if (err) console.error(err)
+    res.render('edit', { restaurant: restaurant })
+  })
+})
+
+// Edit a restaurant info
+app.post('/restaurant/:id', (req, res) => {
+  restaurantList.findById(req.params.id, (err, restaurant) => {
+    if (err) console.error(err)
+    restaurant.name = req.body.name
+    restaurant.name_en = req.body.name_en
+    restaurant.category = req.body.category
+    restaurant.image = req.body.image
+    restaurant.location = req.body.location
+    restaurant.phone = req.body.phone
+    restaurant.google_map = req.body.google_map
+    restaurant.rating = req.body.rating
+    restaurant.description = req.body.description
+
+    restaurant.save(err => {
+      if (err) console.error(err)
+      res.redirect(`/restaurant/${req.params.id}`)
+    })
+  })
 })
 
 // listening on localhost 
